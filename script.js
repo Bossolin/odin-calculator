437858caaeec7a8ec3d138fb077db15663695842
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-04 14:12:00 +0100

    fixed the output

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const output = document.querySelector(".output");
+const result = document.querySelector(".result");
 const inputs = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".operator");
 let num1 = 0;
 let num2 = 0;
-let operation = "";
+let operator = "";
 
 inputs.forEach((input) => {
-  input.addEventListener(
-    "click",
-    (e) => (output.innerText += e.target.innerText)
-  );
+  input.addEventListener("click", (e) => {
+    output.innerText += e.target.innerText;
+  });
 });
 
-operator.forEach(() =>
-  addEventListener("click", (e) => {
+operators.forEach((operator) =>
+  operator.addEventListener("click", (e) => {
     num1 = +output.innerText;
-    operation = e.target.innerText;
+    operator = e.target.innerText;
+    result.innerText = `${num1} ${operator}`;
+    output.innerText = "";
   })
 );
 
@@ -24,8 +26,8 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-function operate(num1, num2, operator) {
-  if (operator == "+") return add(num1, num2);
+function operate(num1, num2, operators) {
+  if (operators == "+") return add(num1, num2);
   else if (operator == "-") return subtract(num1, num2);
   else if (operator == "*") return multiply(num1, num2);
   else if (operator == "/") return divide(num1, num2);
