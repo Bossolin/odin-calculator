b4a06ffa890711b0179783feee22b234d71c1773
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-04 14:38:48 +0100

    managed to make operators do the what i want

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const output = document.querySelector(".output");
 const result = document.querySelector(".result");
 const inputs = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+
 let num1 = 0;
 let num2 = 0;
 let operator = "";
@@ -12,8 +14,8 @@ inputs.forEach((input) => {
   });
 });
 
-operators.forEach((operator) =>
-  operator.addEventListener("click", (e) => {
+operators.forEach((item) =>
+  item.addEventListener("click", (e) => {
     num1 = +output.innerText;
     operator = e.target.innerText;
     result.innerText = `${num1} ${operator}`;
@@ -21,14 +23,16 @@ operators.forEach((operator) =>
   })
 );
 
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+equals.addEventListener("click", () => {
+  num2 = +output.innerText;
+  console.log(operator);
+  result.innerText = `${num1} ${operator} ${num2} =`;
+  operate(num1, num2, operator);
+});
 
-function operate(num1, num2, operators) {
-  if (operators == "+") return add(num1, num2);
-  else if (operator == "-") return subtract(num1, num2);
-  else if (operator == "*") return multiply(num1, num2);
-  else if (operator == "/") return divide(num1, num2);
+function operate(num1, num2, operator) {
+  if (operator == "+") return (output.innerText = num1 + num2);
+  else if (operator == "-") return (output.innerText = num1 - num2);
+  else if (operator == "x") return (output.innerText = num1 * num2);
+  else if (operator == "/") return (output.innerText = num1 / num2);
 }
