d947dab48ef702ed04a951b0451145764f451552
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-04 12:19:06 +0100

    added basic visuals and operate funcs

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("test");
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+function operate(num1, num2, operator) {
+  if (operator == "+") return add(num1, num2);
+  else if (operator == "-") return subtract(num1, num2);
+  else if (operator == "*") return multiply(num1, num2);
+  else if (operator == "/") return divide(num1, num2);
+}
