fe8912999dffabfa8ad09fa08544bd3dd59754bb
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-05 18:52:58 +0100

    added AC and coputed toggle to fix result + new calculation concat

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const result = document.querySelector(".result");
 const inputs = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
+const AC = document.querySelector(".AC");
 
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+let computed = false;
+
+function operate(num1, num2, operator) {
+  if (operator == "+") return (output.innerText = num1 + num2);
+  else if (operator == "-") return (output.innerText = num1 - num2);
+  else if (operator == "x") return (output.innerText = num1 * num2);
+  else if (operator == "/") return (output.innerText = num1 / num2);
+}
 
 inputs.forEach((input) => {
   input.addEventListener("click", (e) => {
+    if (computed) {
+      output.innerText = "";
+      computed = false;
+    }
     output.innerText += e.target.innerText;
   });
 });
@@ -28,11 +41,13 @@ equals.addEventListener("click", () => {
   console.log(operator);
   result.innerText = `${num1} ${operator} ${num2} =`;
   operate(num1, num2, operator);
+  computed = true;
 });
 
-function operate(num1, num2, operator) {
-  if (operator == "+") return (output.innerText = num1 + num2);
-  else if (operator == "-") return (output.innerText = num1 - num2);
-  else if (operator == "x") return (output.innerText = num1 * num2);
-  else if (operator == "/") return (output.innerText = num1 / num2);
-}
+AC.addEventListener("click", () => {
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+  output.innerText = "";
+  result.innerText = "";
+});
