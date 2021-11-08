73f543fe40d6bdc0907ed89ebd85bbf8cf06499e
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-08 11:58:09 +0100

    it just works

--- script.js
+++ script.js
@@ -19,11 +19,88 @@ let computed = false;
 let decimalCheck = true;
 
 function operate(num1, num2, operator) {
-  if (operator == "+") return (num1 += num2);
-  else if (operator == "-") return (num1 -= num2);
-  else if (operator == "x") return (num1 *= num2);
-  else if (operator == "/") return (num1 /= num2);
+  switch (operator) {
+    case "+":
+      return Math.round((num1 += num2) * 100000) / 100000;
+    case "-":
+      return Math.round((num1 -= num2) * 100000) / 100000;
+    case "*":
+      return Math.round((num1 *= num2) * 100000) / 100000;
+    case "/":
+      return Math.round((num1 /= num2) * 100000) / 100000;
+    default:
+      break;
+  }
 }
+
+inputs.forEach((input) => {
+  input.addEventListener("click", (e) => {
+    if (computed) {
+      output.innerText = "";
+      computed = false;
+    }
+    if (output.innerText.length < 10) output.innerText += e.target.innerText;
+  });
+});
+
+operators.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    if (computed) {
+      num1 = +output.innerText;
+      operator = e.target.innerText;
+      computed = false;
+      result.innerText = `${num1} ${operator}`;
+      num2 = 0;
+      output.innerText = "";
+    } else if (num1 && operator) {
+      num2 = +output.innerText;
+      num1 = operate(num1, num2, operator);
+      operator = e.target.innerText;
+      result.innerText = `${num1} ${operator}`;
+      output.innerText = "";
+      computed = true;
+      decimalCheck = true;
+    } else {
+      num1 = +output.innerText;
+      operator = e.target.innerText;
+      result.innerText = `${num1} ${operator}`;
+      output.innerText = "";
+      decimalCheck = true;
+    }
+  })
+);
+
+equals.addEventListener("click", () => {
+  if (!num1 && !operator) return;
+  num2 = +output.innerText;
+  result.innerText = `${num1} ${operator} ${num2} =`;
+  num1 = operate(num1, num2, operator);
+  output.innerText = num1;
+  computed = true;
+  decimalCheck = true;
+});
+
+aC.addEventListener("click", () => {
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+  computed = false;
+  decimalCheck = true;
+  output.innerText = "";
+  result.innerText = "";
+});
+
+decimal.addEventListener("click", (e) => {
+  if (decimalCheck) output.innerText += e.target.innerText;
+  decimalCheck = false;
+});
+
+c.addEventListener("click", () => {
+  if (output.innerText.slice(-1) == ".") {
+    decimalCheck = true;
+  }
+  output.innerText = output.innerText.substring(0, output.innerText.length - 1);
+});
 
 window.addEventListener("keydown", (e) => {
   console.log(e.key);
@@ -37,7 +114,14 @@ window.addEventListener("keydown", (e) => {
     if (decimalCheck) output.innerText += e.key;
     decimalCheck = false;
   } else if (opValues.includes(e.key)) {
-    if (num1 && operator) {
+    if (computed) {
+      num1 = +output.innerText;
+      operator = e.key;
+      computed = false;
+      result.innerText = `${num1} ${operator}`;
+      num2 = 0;
+      output.innerText = "";
+    } else if (num1 && operator) {
       num2 = +output.innerText;
       num1 = operate(num1, num2, operator);
       operator = e.key;
@@ -54,7 +138,7 @@ window.addEventListener("keydown", (e) => {
     }
   } else if (equals.innerText == e.key) {
     if (!num1 && !operator) return;
-    if (!num2) num2 = +output.innerText;
+    num2 = +output.innerText;
     result.innerText = `${num1} ${operator} ${num2} =`;
     num1 = operate(num1, num2, operator);
     output.innerText = num1;
@@ -85,66 +169,4 @@ window.addEventListener("keydown", (e) => {
     output.innerText = "";
     result.innerText = "";
   }
-});
-
-inputs.forEach((input) => {
-  input.addEventListener("click", (e) => {
-    if (computed) {
-      output.innerText = "";
-      computed = false;
-    }
-    if (output.innerText.length < 10) output.innerText += e.target.innerText;
-  });
-});
-
-operators.forEach((item) =>
-  item.addEventListener("click", (e) => {
-    if (num1 && operator) {
-      num2 = +output.innerText;
-      num1 = operate(num1, num2, operator);
-      operator = e.target.innerText;
-      result.innerText = `${num1} ${operator}`;
-      output.innerText = "";
-      computed = true;
-      decimalCheck = true;
-    } else {
-      num1 = +output.innerText;
-      operator = e.target.innerText;
-      result.innerText = `${num1} ${operator}`;
-      output.innerText = "";
-      decimalCheck = true;
-    }
-  })
-);
-
-equals.addEventListener("click", () => {
-  if (!num1 && !operator) return;
-  if (!num2) num2 = +output.innerText;
-  result.innerText = `${num1} ${operator} ${num2} =`;
-  num1 = operate(num1, num2, operator);
-  output.innerText = num1;
-  computed = true;
-  decimalCheck = true;
-});
-
-aC.addEventListener("click", () => {
-  num1 = 0;
-  num2 = 0;
-  operator = "";
-  computed = false;
-  decimalCheck = true;
-  output.innerText = "";
-  result.innerText = "";
-});
-
-decimal.addEventListener("click", (e) => {
-  if (decimalCheck) output.innerText += e.target.innerText;
-  decimalCheck = false;
-});
-
-c.addEventListener("click", () => {
-  if (output.innerText.slice(-1) == ".") {
-    decimalCheck = true;
-  }
-  output.innerText = output.innerText.substring(0, output.innerText.length - 1);
 });
